ac2c1d306e7dd04b47377b33a48c6b57008b450f
Ruari Day <ruariday@gmail.com>
2020-03-09 13:09:21 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,12 @@
-def square_array(array)
-  array.each {|num| num ** 2}
-end
-
+#intake number, square it, return new array with squared numbers
 
 
 def square_array(array)
   array.each {|num| num ** 2}
 end
+
+
+
+#def square_array(array)
+  #array.each {|num| num ** 2}
+#end
